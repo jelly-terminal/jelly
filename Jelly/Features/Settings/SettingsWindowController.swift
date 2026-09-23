a543ef6b7e947c4c77869ba8c0969c6fba4c9f05
mxvsh <xencodes@icloud.com>
2026-09-23 15:30:13 +0530

    fix: size the Settings window to fit its pages

--- Jelly/Features/Settings/SettingsWindowController.swift
+++ Jelly/Features/Settings/SettingsWindowController.swift
@@ -3,12 +3,17 @@ import SwiftUI
 
 final class SettingsWindowController: NSWindowController {
     init(configStore: ConfigStore, updatesAvailable: Bool, onCheckForUpdates: @escaping () -> Void, onImportTheme: @escaping () -> Void) {
+        let size = NSSize(width: Metrics.settingsWidth, height: Metrics.settingsHeight)
         let tabs = NSTabViewController()
         tabs.tabStyle = .toolbar
+        tabs.transitionOptions = []
 
         func add(_ title: String, symbol: String, _ view: some View) {
             let hosting = NSHostingController(rootView: view)
             hosting.title = title
+            hosting.sizingOptions = []
+            hosting.preferredContentSize = size
+            hosting.view.frame = NSRect(origin: .zero, size: size)
             let item = NSTabViewItem(viewController: hosting)
             item.label = title
             item.image = NSImage(systemSymbolName: symbol, accessibilityDescription: title)
@@ -29,8 +34,8 @@ final class SettingsWindowController: NSWindowController {
         window.styleMask = [.titled, .closable, .miniaturizable]
         window.toolbarStyle = .preference
         window.isReleasedWhenClosed = false
+        window.setContentSize(size)
         window.center()
-        window.setFrameAutosaveName("JellySettingsWindow")
         super.init(window: window)
     }
 
